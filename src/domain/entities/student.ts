bc88c8f9f836295f4c79809b99262383c61a5d56
Andressa Lessa <s.andressalessa@gmail.com>
2026-07-21 19:09:57 -0300

    Adding lint configs

--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,21 +1,18 @@
-import { Entity } from "@/core/entities/entity.js";
-import type { UniqueEntityID } from "@/core/entities/unique-entity-id.js";
+import { Entity } from '@/core/entities/entity.js'
+import type { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
 
 interface StudentProps {
-    name: string
+  name: string
 }
 
 export class Student extends Entity<StudentProps> {
-    get name() {
-        return this.props.name;
-    }
+  get name() {
+    return this.props.name
+  }
 
-    static create(
-            props: StudentProps,
-            id: UniqueEntityID
-        ) {
-            const student = new Student(props, id);
+  static create(props: StudentProps, id: UniqueEntityID) {
+    const student = new Student(props, id)
 
-            return student;
-    }
+    return student
+  }
 }
--- src/domain/entities/value-objects/slug.spec.ts
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -1,7 +1,7 @@
-import { expect, test } from "vitest";
-import { Slug } from "./slug.js";
+import { expect, test } from 'vitest'
+import { Slug } from './slug.js'
 
 test('it should be able to create a new slug from text', () => {
-    const slug = Slug.createFromText('This is a title');
-    expect(slug.value).toBe('this-is-a-title');
-});
+  const slug = Slug.createFromText('This is a title')
+  expect(slug.value).toBe('this-is-a-title')
+})
